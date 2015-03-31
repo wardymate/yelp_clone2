8075f5e92eeb6d7406611d53799c277db5dfb5b0
wardymate <chriswarddev@gmail.com>
2015-03-31 15:23:54 +0100

    review is deleted when restaurant is deleted

--- spec/features/reviews_spec.rb
+++ spec/features/reviews_spec.rb
@@ -4,14 +4,27 @@ feature 'reviewing' do
 
   before {Restaurant.create name: 'KFC'}
 
-  scenario 'allow users to leave a review' do
+  def add_review
     visit '/restaurants'
-    click_link 'Review KFC'
-    fill_in 'Thoughts', with: "so so"
-    select '3', from: "Rating"
-    click_button 'Leave Review'
-    expect(current_path).to eq '/restaurants'
-    expect(page).to have_content('so so')
+     click_link 'Review KFC'
+     fill_in "Thoughts", with: "so so"
+     select '3', from: 'Rating'
+     click_button 'Leave Review'
+     expect(current_path).to eq '/restaurants'
+     expect(page).to have_content('so so')
+  end
+
+
+  scenario 'allow users to leave a review' do
+    add_review
+  end
+
+
+  scenario 'deletes a review when a restaurant is deleted' do
+    add_review
+    click_link "Delete KFC"
+    expect(page).not_to have_content 'KFC'
+    expect(page).to have_content 'Restaurant deleted successfully'
   end
 
 end
